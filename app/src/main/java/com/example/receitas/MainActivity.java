20b0e5e36af15645b502c47ed261b487f28a61f2
Luana Assis Silva <88562681+luanaassis@users.noreply.github.com>
2023-07-14 09:38:45 -0300

    Update MainActivity.java

--- app/src/main/java/com/example/receitas/MainActivity.java
+++ app/src/main/java/com/example/receitas/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import java.io.IOException;
 
@@ -18,7 +19,12 @@ public class MainActivity extends AppCompatActivity {
         app.executor.execute(() -> {
             try {
                 Meals receita = app.getReceitasRepo().buscaReceita();
-                Log.d("Receita", receita.strMeal);
+                for(int i = 0; i< receita.meals.length;i++) {
+                    TextView nomeReceita = findViewById(R.id.nomeReceita);
+                    nomeReceita.setText(String.valueOf(receita.meals[0].strMeal));
+                    Log.d("ReceitaRETORNO", String.valueOf(receita.meals[0].strMeal));
+                    Log.d("ReceitaRETORNO", String.valueOf(receita.meals[0].strIngredient1));
+                }
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
