676ffa2aca3692eca4e9420eaf98350555e115d9
Luana Assis Silva <88562681+luanaassis@users.noreply.github.com>
2023-07-14 10:27:58 -0300

    Update MainActivity.java

--- app/src/main/java/com/example/receitas/MainActivity.java
+++ app/src/main/java/com/example/receitas/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.receitas;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.MutableLiveData;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -9,21 +10,24 @@ import android.widget.TextView;
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
-
+    private MutableLiveData<String> ingrediente1 = new MutableLiveData<>();
+    private TextView ingredientes;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ReceitaApplication app = (ReceitaApplication)getApplication();
+        ingredientes = (TextView) findViewById(R.id.textView);
+
+        ingrediente1.observe(this, (v) -> ingredientes.setText(v));
 
         app.executor.execute(() -> {
             try {
                 Meals receita = app.getReceitasRepo().buscaReceita();
                 for(int i = 0; i< receita.meals.length;i++) {
-                    TextView nomeReceita = findViewById(R.id.nomeReceita);
-                    nomeReceita.setText(String.valueOf(receita.meals[0].strMeal));
                     Log.d("ReceitaRETORNO", String.valueOf(receita.meals[0].strMeal));
                     Log.d("ReceitaRETORNO", String.valueOf(receita.meals[0].strIngredient1));
+                    ingrediente1.postValue(String.valueOf(receita.meals[0].strIngredient1+" \n"+ receita.meals[0].strIngredient2+" \n"));
                 }
             } catch (IOException e) {
                 throw new RuntimeException(e);
